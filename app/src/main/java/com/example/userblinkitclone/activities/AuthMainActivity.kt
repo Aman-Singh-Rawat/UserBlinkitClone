1c960e769d2a8b1e9ec3b42e0b32db0a404efdbd
Aman Singh <amanrawat6767@gmail.com>
2024-09-28 10:07:12 +0530

    refactor of AuthMainActivity

--- app/src/main/java/com/example/userblinkitclone/activities/AuthMainActivity.kt
+++ app/src/main/java/com/example/userblinkitclone/activities/AuthMainActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.userblinkitclone.R
 
-class MainActivity : AppCompatActivity() {
+class AuthMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
